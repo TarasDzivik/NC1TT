a2821dbfb56f116576916565edeefdff97abdf71
TarasDzivik <t.dzivik@gmail.com>
2022-12-20 13:51:11 +0100

    Add project files.

---
+++ Entities/ProgrammingLanguage.cs
@@ -0,0 +1,12 @@
+﻿namespace NC1TestTask.Entities;
+
+/// <summary>
+/// The object contains the name of the Programming language.
+/// </summary>
+public class ProgrammingLanguage
+{
+    /// <summary>
+    /// The name of the Programming language.
+    /// </summary>
+    public string Name { get; set; }
+}
