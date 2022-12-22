7656fd0b5d0a67d0179a7ca6fdec818cb0f72fa7
TarasDzivik <t.dzivik@gmail.com>
2022-12-22 17:55:37 +0100

    Played with Automapper and FluentValidation.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,19 @@
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using NC1TestTask.Data;
+using NC1TestTask.Srvices.Validation;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection")));
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+//builder.Services.AddValidatorsFromAssemblyContaining<EmployeeValidation>(lifetime: ServiceLifetime.Transient);
+builder.Services.AddControllers();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -22,11 +27,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
